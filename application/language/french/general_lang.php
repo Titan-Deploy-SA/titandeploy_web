14107632738b8aad3ac67a1206a278ecd82b80c9
rupall99 <rupal.rai@titandeploy.com>
2025-04-24 18:38:03 +0530

    final home page, about, all  service, blog, contact, footer, nav bar

--- application/language/french/general_lang.php
+++ application/language/french/general_lang.php
@@ -3,3 +3,22 @@ $lang['welcome_message'] = "Welcome to our website!";
 $lang['login_button'] = "Login";
 $lang['logout_button'] = "Logout";
 $lang["language_label"] = "Langage";
+$lang["nav_contact_email"] = "contact@titandeploy.com";
+$lang["nav_home"] = "Accueil";
+$lang["nav_about"] = "Infos";
+$lang["nav_services"] = "Services";
+$lang["nav_blog"] = "Blog";
+$lang["nav_contact"] = "Contact";
+$lang["nav_buttton_get_quotation"] = "Obtenir devis";
+$lang["nav_call_expert"] = "Contacter un expert";
+$lang["nav_free"] = "Gratuit : +91 90353 25481";
+
+//footer
+$lang["footer_titandeploy"] = "TitanDeploy simplifie le déploiement avec des outils fluides pour une montée en charge rapide, une surveillance en temps réel et des mises à jour efficaces. Intuitif et fiable pour les développeurs.";
+$lang["footer_useful_links"] = "Liens utiles";
+$lang["footer_links_home"] = "Accueil";
+$lang["footer_links_about"] = "À propos";
+$lang["footer_links_services"] = "Services";
+$lang["footer_links_blog"] = "Blogs";
+$lang["footer_links_contact"] = "Contact";
+$lang["footer_mail_us"] = "Contactez-nous";
